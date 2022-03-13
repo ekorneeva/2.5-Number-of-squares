96d333ecafddbdd5c15e4dac0a15ed5b064eab90
ekorneeva <43140015+ekorneeva@users.noreply.github.com>
2022-03-13 13:03:15 +0300

    Update SQRServiceTest.java

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -21,4 +21,43 @@ class SQRServiceTest {
 
         assertEquals(expected, actual);
     }
+    
+        @Test
+    void shouldSqrNumber() {
+        SQRService service = new SQRService();
+
+        int bottom = 2025;
+        int upper = 2500;
+        int expected = 6;
+
+        int actual = service.findNumber(bottom, upper);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldSqrNumber() {
+        SQRService service = new SQRService();
+
+        int bottom = 100;
+        int upper = 200;
+        int expected = 5;
+
+        int actual = service.findNumber(bottom, upper);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldSqrNumber() {
+        SQRService service = new SQRService();
+
+        int bottom = 9604;
+        int upper = 9801;
+        int expected = 2;
+
+        int actual = service.findNumbers(bottom, upper);
+
+        assertEquals(expected, actual);
+    }
 }
